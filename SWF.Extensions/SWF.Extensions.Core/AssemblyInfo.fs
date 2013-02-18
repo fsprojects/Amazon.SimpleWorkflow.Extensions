5ddfb37009904823ffadc4f7b9c05d9963a265f6
Yan Cui <theburningmonk@gmail.com>
2013-02-18 17:17:36 +0000

    - added AssemblyInfo

---
+++ SWF.Extensions/SWF.Extensions.Core/AssemblyInfo.fs
@@ -0,0 +1,17 @@
+﻿namespace Amazon.SimpleWorkflow.Extensions
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: AssemblyTitle("SWF.Extensions.Core.dll")>]
+[<assembly: AssemblyDescription("Provides an intuitive API for modelling workflows against SWF")>]
+
+[<assembly: AssemblyProduct("Amazon.SimpleWorkflow.Extensions")>]
+[<assembly: AssemblyCopyright("Copyright @ Yan Cui 2013")>]
+[<assembly: AssemblyVersion("1.0.2")>]
+[<assembly: AssemblyFileVersion("1.0.2")>]
+
+do ()
