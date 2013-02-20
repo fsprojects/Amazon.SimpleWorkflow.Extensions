6fada5c57c89a1c011abecc86c89b38776c04196
theburningmonk <theburningmonk@gmail.com>
2013-02-20 00:45:55 +0000

    - updated to nuget v1.0.3

--- SWF.Extensions/SWF.Extensions.Core/AssemblyInfo.fs
+++ SWF.Extensions/SWF.Extensions.Core/AssemblyInfo.fs
@@ -11,7 +11,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyProduct("Amazon.SimpleWorkflow.Extensions")>]
 [<assembly: AssemblyCopyright("Copyright @ Yan Cui 2013")>]
-[<assembly: AssemblyVersion("1.0.2")>]
-[<assembly: AssemblyFileVersion("1.0.2")>]
+[<assembly: AssemblyVersion("1.0.3")>]
+[<assembly: AssemblyFileVersion("1.0.3")>]
 
 do ()
