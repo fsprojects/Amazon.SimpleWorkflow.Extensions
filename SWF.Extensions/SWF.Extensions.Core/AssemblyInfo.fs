6ef44e2e2cf7d5a22f612ce7ce47c0c8ed299c4f
theburningmonk <theburningmonk@gmail.com>
2013-03-15 00:03:02 +0000

    - updated assembly info - updated nuget package to v1.1.0

--- SWF.Extensions/SWF.Extensions.Core/AssemblyInfo.fs
+++ SWF.Extensions/SWF.Extensions.Core/AssemblyInfo.fs
@@ -6,12 +6,13 @@ open System.Runtime.InteropServices
 
 [<assembly: ComVisible(false)>]
 
-[<assembly: AssemblyTitle("SWF.Extensions.Core.dll")>]
+[<assembly: AssemblyTitle("SWF.Extensions.Core")>]
 [<assembly: AssemblyDescription("Provides an intuitive API for modelling workflows against SWF")>]
+[<assembly: AssemblyConfiguration("theburningmonk.com")>]
 
 [<assembly: AssemblyProduct("Amazon.SimpleWorkflow.Extensions")>]
-[<assembly: AssemblyCopyright("Copyright @ Yan Cui 2013")>]
-[<assembly: AssemblyVersion("1.0.4")>]
-[<assembly: AssemblyFileVersion("1.0.4")>]
+[<assembly: AssemblyCopyright("Copyright © Yan Cui 2013")>]
+[<assembly: AssemblyVersion("1.1.0")>]
+[<assembly: AssemblyFileVersion("1.1.0")>]
 
 do ()
--- SWF.Extensions/SWF.Extensions.CoreCs/Properties/AssemblyInfo.cs
+++ SWF.Extensions/SWF.Extensions.CoreCs/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SWF.Extensions.CoreCs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides an intuitive API for modelling workflows against SWF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SWF.Extensions.CoreCs")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("theburningmonk.com")]
+[assembly: AssemblyProduct("Amazon.SimpleWorkflow.Extensions")]
+[assembly: AssemblyCopyright("Copyright © Yan Cui 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
